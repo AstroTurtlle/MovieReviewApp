64e3d9d38f7742366e82279efbdecde3cae3b6a4
Stefancu Alexandru <Stefancu.Io.Gabriel@student.utcluj.ro>
2024-06-02 22:49:20 +0300

    crud filme+iconita profil si drop down menu

---
+++ react_app/src/components/ProfileIcon.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { FaUser, FaBookmark, FaSignOutAlt } from 'react-icons/fa';
+import './ProfileIcon.css';
+
+const ProfileIcon = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
+  return (
+    <div className="profile-icon-container">
+      <FaUser onClick={toggleMenu} style={{ marginTop: '-5px' }} />
+      {showMenu && (
+        <div className="dropdown-menu" onBlur={closeMenu} tabIndex={0}>
+          <div className="dropdown-item"><FaUser/>Profile</div>
+          <div className="dropdown-item"><FaBookmark/>Bookmark Films</div>
+          <div className="dropdown-item"><FaSignOutAlt/>Log Out</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProfileIcon;
